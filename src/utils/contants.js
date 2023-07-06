e0be583eb9816ad85dfd40b6ee4cfe4a0ca19d85
nikhilJgit <121334445+nikhilJgit@users.noreply.github.com>
2023-07-06 20:20:43 +0530

    Add files via upload

---
+++ src/utils/contants.js
@@ -0,0 +1,13 @@
+const GOOGLE_API_KEY = "AIzaSyAn8t9rCVayRJc8ytfrkHtuY76H5UPa5kk";
+
+export const LIVE_CHAT_COUNT = 25;
+
+export const YOUTUBE_VIDEOS_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+  GOOGLE_API_KEY;
+
+export const YOUTUBE_SEARCH_API =
+
+  "http://suggestqueries.google.com/complete/search?client=firefox&q=";
+
+// Live Chat >>>> Infinite Scroll >>>>>> Pagination
